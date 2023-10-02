2c82f1c31af4e84d02c087f710fb3b05207a743c
Broly366 <erbek00@outlook.it>
2023-10-02 06:47:55 +0200

    added buttons and basic user interface
    added the update grid button that if num is put inside the input and button is clicked the grid is cleaned and resied.
    i added also a clear button that removes all the painting.

--- javascript.js
+++ javascript.js
@@ -6,7 +6,39 @@ let divCount = 0;
 
 const squares = document.querySelectorAll(".square");
 
-function createDivGrid(num) {
+let isMouseDown = false;
+
+
+
+container.addEventListener("mousedown", (event) => {
+    if (event.target.classList.contains("square")) {
+        isMouseDown = true;
+        event.target.style.backgroundColor = "black";
+    }
+});
+
+container.addEventListener("mouseover", (event) => {
+    if (isMouseDown && event.target.classList.contains("square") && event.target.style.backgroundColor !== "black") {
+        event.target.style.backgroundColor = "black";
+    }
+});
+
+container.addEventListener("mouseleave", () => {
+    isMouseDown = false;
+});
+
+document.addEventListener("mouseup", () => {
+    isMouseDown = false;
+});
+
+
+
+function createDivGrid(num = 16) {
+    const removeExistingSquares = document.querySelectorAll(".square");
+    removeExistingSquares.forEach((square) => {
+        square.remove();
+    });
+
     for (let i = 0;i < num; i++) {
         for (let j = 0; j < num; j++) {
             const div = document.createElement("div");
@@ -22,44 +54,46 @@ function createDivGrid(num) {
     }; 
 }
 
-createDivGrid(25);
+createDivGrid();
 
-let isMouseDown = false;
-
-
-function hoverEffect() {
-    const squares = document.querySelectorAll(".square");
-    squares.forEach((square) => {
-        square.addEventListener("mouseup", () => {
-            isMouseDown = false;
-        });
-        square.addEventListener("mousedown", (event) => {
-            isMouseDown = true;
-            square.style.backgroundColor = "black";
-        });
-        square.addEventListener("mouseover", () => {
-            if (isMouseDown && square.style.backgroundColor !== "black") {
-                square.style.backgroundColor = "black";
-            };
-        });
-    });
-};
-
-hoverEffect();
-
-container.addEventListener("mouseleave", () => {
-    isMouseDown = false;
-});
-
-document.addEventListener("mouseup", () => {
-    isMouseDown = false;
-});
+const buttons = document.querySelector(".buttons");
 
 const header = document.querySelector(".header");
 
-const gridButton = document.createElement("button");
-gridButton.classList.add("gridButton")
-gridButton.textContent = "Input grid size";
+const gridInputButton = document.createElement("button");
+gridInputButton.textContent = "Update grid";
 
-header.appendChild(gridButton);
+buttons.appendChild(gridInputButton);
+
+const gridInput = document.createElement("input");
+gridInput.classList.add("gridInput")
+gridInput.value = "";
+
+buttons.appendChild(gridInput);
+
+const clearButton = document.createElement("button");
+clearButton.textContent = "Clear";
+
+buttons.appendChild(clearButton);
+
+clearButton.addEventListener("click", () => {
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+      square.style.backgroundColor = "white";
+    });
+  });
+
+gridInputButton.addEventListener("click", () => {
+    const gridSize = parseInt(gridInput.value);
+
+        if (!isNaN(gridSize)) {
+            createDivGrid(gridSize);
+        } else {
+            alert("please enter a valid number");
+        }
+    });
+
+
+
 });
+
